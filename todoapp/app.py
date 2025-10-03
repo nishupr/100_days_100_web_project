d1d81bd9364b27883b819dfa50ebbca648b081bc
Shaurya Jadaun <shauryajadaun3@gmail.com>
2025-10-03 19:50:11 +0530

    database tables are added

---
+++ todoapp/app.py
@@ -0,0 +1,22 @@
+from flask import Flask ,request,render_template,redirect,session,flash
+from flask_sqlalchemy import SQLAlchemy
+import bcrypt
+
+app = Flask(__name__)
+app.secret_key = 'secret_key'
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///datausers.db'
+db = SQLAlchemy(app)
+
+
+class Task(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(100), unique=True, nullable=False)
+    password = db.Column(db.String(100), nullable=False)
+    tasks = db.relationship('Task', backref='user', lazy=True)
