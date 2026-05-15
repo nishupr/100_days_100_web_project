5a885246c47f82dec7e329a684d4129065c32f92
Sameer <sameer12.gera@gmail.com>
2026-05-15 13:23:44 +0530

    Fixed relative pathing

--- index.js
+++ index.js
@@ -312,7 +312,7 @@ function fillTable() {
         ["Day 113", "CPU Scheduler", "./public/CpuScheduler/index.html"],
         ["Day 114", "EchoNotes", "./public/EchoNotes/index.html"],
         ["Day 115", "Event Registration System", "https://event-registration-system-w10a.onrender.com/"],
-        ["Day 116", "AI Image Classifier", "/public/AI Image CLassifier/index.html"],
+        ["Day 116", "AI Image Classifier", "./public/AI Image CLassifier/index.html"],
         ["Day 117", "ZEN TIMER", "./public/ZEN_TIMER/index.html"],
     ];
     const tbody = document.getElementById('tableBody');
