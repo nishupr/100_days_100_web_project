ae0bbdf025ed9c5d3cb69e38225d22822f1a4ce2
kavita bande <kavitabande2006@gmail.com>
2026-05-18 14:13:03 +0530

    Added loading spinner during weather fetch

--- public/Weather Forcasting/script.js
+++ public/Weather Forcasting/script.js
@@ -8,11 +8,12 @@ const options = {
 };
 const getWeather = (city) => {
     cityName.innerHTML = city
+    loading.style.display = "block";
     fetch('https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=' + city, options)
         .then(response => response.json())
         .then((response) => {
             console.log(response)
-
+            loading.style.display = "none";
             // cloud_pct.innerHTML = response.cloud_pct
             temp.innerHTML = response.temp
             temp2.innerHTML = response.temp
@@ -26,9 +27,16 @@ const getWeather = (city) => {
             wind_degrees.innerHTML = response.wind_degrees
             sunrise.innerHTML = response.sunrise
             sunset.innerHTML = response.sunset
+            
 
         })
-        .catch(err => console.error(err));
+          .catch(err => {
+
+            // HIDE spinner if error occurs
+            loading.style.display = "none";
+
+            console.error(err);
+        });
 
 }
 
